ff172d14c462426b22a9f8983749daf9b8fa7d92
Thiago Ferreira <thiago-f_4@outlook.com>
2018-10-06 23:46:00 -0300

    06-10

--- avaliacoes-frontend/angular/src/app/list/list.page.ts
+++ avaliacoes-frontend/angular/src/app/list/list.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {legends} from '../names/nameslist';
+import { legends } from '../names/nameslist';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +7,29 @@ import {legends} from '../names/nameslist';
   styleUrls: ['list.page.scss']
 })
 export class ListPage {
-  legends: Array<string> = [];  
+  legends: Array<string> = [];
+  isEditing: boolean;
+  name: String;
+  nameEdited: String;
 
-  ngOnInit(){
-	  this.legends = legends;
-  }  
+  ngOnInit() {
+    this.legends = legends;
+    this.isEditing = false;
+  }
+
+  edit(item: string): void {
+    this.name = '';
+    this.isEditing = true;
+    this.name = item;
+    this.nameEdited = item;
+  }
+
+  save(itemEdited: string, item: string): void {
+    let index = this.legends.indexOf(item);
+    legends.splice(index, 1, itemEdited);
+  }
+
+  closeSave() {
+    this.name = '';
+  }
 }
