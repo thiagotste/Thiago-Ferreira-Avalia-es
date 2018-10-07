bae92d7a24a89a1a2fc3f1fe17ad6ec5377365a3
Thiago Ferreira <thiago-f_4@outlook.com>
2018-10-07 19:26:39 -0300

    07-10

--- avaliacoes-frontend/angular/src/app/edit/edit.page.ts
+++ avaliacoes-frontend/angular/src/app/edit/edit.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { legends } from '../names/nameslist';
 
 @Component({
   selector: 'app-edit',
@@ -6,11 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['edit.page.scss']
 })
 export class EditPage {
+  legends: Array<string> = [];
+  isEditing: boolean;
+  name: String;
+  nameEdited: String;
 
   constructor() {
-    
+    this.legends = legends;
+    this.isEditing = false;
   }
 
+  edit(item: string): void {
+    this.name = '';
+    this.isEditing = true;
+    this.name = item;
+    this.nameEdited = item;
+  }
+
+  save(itemEdited: string, item: string): void {
+    let index = this.legends.indexOf(item);
+    legends.splice(index, 1, itemEdited);
+  }
+
+  closeSave() {
+    this.name = '';
+  }
 }
 
 
--- avaliacoes-frontend/angular/src/app/list/list.page.ts
+++ avaliacoes-frontend/angular/src/app/list/list.page.ts
@@ -8,28 +8,8 @@ import { legends } from '../names/nameslist';
 })
 export class ListPage {
   legends: Array<string> = [];
-  isEditing: boolean;
-  name: String;
-  nameEdited: String;
 
   ngOnInit() {
     this.legends = legends;
-    this.isEditing = false;
-  }
-
-  edit(item: string): void {
-    this.name = '';
-    this.isEditing = true;
-    this.name = item;
-    this.nameEdited = item;
-  }
-
-  save(itemEdited: string, item: string): void {
-    let index = this.legends.indexOf(item);
-    legends.splice(index, 1, itemEdited);
-  }
-
-  closeSave() {
-    this.name = '';
   }
 }
